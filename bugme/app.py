566acf99dcbe527731b4fbe7eee55cda74799191
Ramasubramanian S <sramsubu@gmail.com>
2020-02-01 23:09:06 +0530

    Refactored the code

--- bugme/app.py
+++ bugme/app.py
@@ -1,79 +1,152 @@
-from db import *
+import sqlite3
 from pathlib import Path
+from functools import wraps
 
-from app_config import DB_PATH
+APP_PATH = '.bugme'
+DB_PATH = '{APP_PATH}/bugs.db'
+
+def get_connection(db_path):
+    con = sqlite3.connect(db_path, isolation_level=None)
+
+    def dict_factory(cursor, row):
+        return {col[0]: row[idx] for idx, col in enumerate(cursor.description)}
+
+    con.row_factory = dict_factory
+    return con
+
+def create_database_tables(db_path):
+    con = get_connection(db_path)
+    with open("ddl.sql") as fp:
+        sql_script = fp.read()
+
+    con.executescript(sql_script)
+
+def generate_update_query(tablename, record_dict, pk_name, pk_value):
+    query_template = 'UPDATE {table} SET {update_string} WHERE {pk_name}=?;'
+    update_string = ','.join('{name}=?'.format(name=name) for name in record_dict)
+    row = tuple(record_dict.values()) + (pk_value, )
+    query = query_template.format(table=tablename,
+                                  update_string=update_string,
+                                  pk_name=pk_name)
+    return query, row
 
 
+def generate_insert_query(tablename, record_dict):
+    query_template = 'INSERT INTO {table}({names}) VALUES({values});'
+    names = ','.join(record_dict.keys())
+    values = ','.join('?' for _ in record_dict)
+    row = tuple(record_dict.values())
+    query = query_template.format(table=tablename, names=names, values=values)
+    return query, row
 
-class App:
-    def __init__(self, instance_path):
-        self.app_path = instance_path
-        self.db_path = DB_PATH
-        self.db = get_connection(self.db_path)
+
+def get_app_instance():
+    home = Path.home()
+    cwd = Path.cwd()
+
+    while cwd != home:
+        path = cwd / APP_PATH
+        if path.exists():
+            return path
+        cwd = cwd.parent
+
+    return None
 
 
-    @classmethod
-    def create_new_instance(cls, path):
-        path = Path(path) / ".bugme"
-        path.mkdir()
+def require_app_instance(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        inst = get_app_instance()
+        if inst is None:
+            raise RuntimeError("No app instance found")
+        kwargs.update(instance=inst)
+        return func(*args, **kwargs)
+    return wrapper
 
-        db_file = path / "bugs.db"
-        db_file.touch()
 
-        con = get_connection(str(db_file))
-        sync_db(con)
-        con.close()        
+def create_new_instance(path=None):
+    if path is None:
+        path = Path.cwd()
+    path = path / APP_PATH
+    path.mkdir()
+    db_path = DB_PATH.format(APP_PATH=path)
+    create_database_tables(db_path)
+
+
+def create_new_defect(description, observed, expected, instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+    record = {"description": description,
+              "expected_behaviour": expected,
+              "observed_behaviour": observed,
+              "status" : "OPEN"}
+    query, row = generate_insert_query("bugs", record)
+    res = dbcon.execute(query, row)
+    return res.lastrowid
+
+def update_status(defect_id, status, instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+    record = {"status": status}
+    query, row = generate_update_query("bugs", record, "defect_id", defect_id)
+    try:
+        dbcon.execute(query, row)
+    except sqlite3.IntegrityError as e:
+        print("Invalid status value provided")
+
         
-    def create_new_defect(self, description, expected_behaviour, observed_behaviour):
-        record = {
-            "description": description,
-            "expected_behaviour": expected_behaviour,
-            "observed_behaviour": observed_behaviour,
-            "status": "OPEN"
-        }
-        query, row = generate_insert_query("bugs", record)
-        return self.db.execute(query, row).lastrowid
-
-    def get_defect(self, defect_id):
-        query = 'SELECT * FROM bugs WHERE defect_id=?;'
-        row = (defect_id, )
-        defect = self.db.execute(query, row).fetchall()
-        if len(defect) < 1:
-            raise ValueError(f"No record found for the defect with id {defect_id}")
-        defect = defect[0]
-        query = "SELECT * FROM comments WHERE defect_id=?;"
-        comments =self.db.execute(query, row).fetchall()
-        defect['comments'] = comments
-        return defect
-
-
-    def update_status(self, defect_id, new_status):
-        query = 'UPDATE bugs SET status=? WHERE defect_id=?;'
-        row = (new_status, defect_id)
-        self.db.execute(query,row)
-
-
-    def add_comment(self, defect_id, comments):
-        record = {
-            "defect_id": defect_id,
-            "comment": comments
-        }
-        query, row = generate_insert_query('comments', record)
-        self.db.execute(query, row)
-
-    def resolve_defect(self, defect_id, resolution):
-        query = "UPDATE bugs SET status='CLOSED', resolution=?, resolution_date=CURRENT_TIMESTAMP WHERE defect_id=?;"
-        row = (resolution, defect_id)
-        self.db.execute(query, row)
-
-    def get_all_defects():
-        query = 'SELECT * FROM bugs;'
-        return self.db.execute(query)
-
-
-
-
-if __name__ == '__main__':
-    defect_id = int(input('Defectid: '))
-    get_defect(defect_id)
+def get_status(defect_id, instance):
+    defect_id = int(defect_id)
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+    query = "SELECT status FROM bugs WHERE defect_id=?;"
+    row = (defect_id, )
+    res = dbcon.execute(query, row).fetchall()
+    return res[0]['status']
     
+    
+    
+def get_all_bugs(instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+    query = 'SELECT * FROM bugs;'
+    bugs = dbcon.execute(query).fetchall()
+    return bugs
+
+
+def get_bug(defect_id, instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+    query = 'SELECT * FROM bugs WHERE defect_id=?;'
+    row = (defect_id, )
+    bug = dbcon.execute(query, row).fetchall()
+    bug = bug[0]
+
+    comments = 'SELECT * FROM comments WHERE defect_id=?;'
+    row = (defect_id, )
+    comments = dbcon.execute(comments, row).fetchall()
+    bug['comments'] = comments
+    return bug
+
+
+def add_comment(defect_id, comment, instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+
+    record = {"defect_id": defect_id, "comment": comment}
+    query, row = generate_insert_query("comments", record)
+    cid = dbcon.execute(query, row)
+    return cid
+    
+
+def resolve(defect_id, resolution, instance):
+    db_path = DB_PATH.format(APP_PATH=instance)
+    dbcon = get_connection(db_path)
+
+    record = {"resolution": resolution,  "status": "CLOSED"}
+    query, row = generate_update_query("bugs", record, "defect_id", defect_id)
+    dbcon.execute(query, row)
+
+    query = 'UPDATE bugs SET resolution_date=CURRENT_TIMESTAMP WHERE defect_id=?;'
+    row = (defect_id, )
+    dbcon.execute(query, row)
--- bugme/cli.py
+++ bugme/cli.py
@@ -1,133 +1,189 @@
 from argparse import ArgumentParser
 from pathlib import Path
 import sys
+from shutil import make_archive
 
-from app_config import APP_PATH
 import app
 
+DESCRIPTION = "bugme lightweight database"
+PROG = "bugme"
+VERSION = '0.1'
+
+
+def get_multiline(prompt):
+    lines = []
+    print(prompt)
+    print("Press Ctrl+D to end")
+    while True:
+        try:
+            line = input()
+            lines.append(line)
+        except EOFError:
+            break
+    return '\n'.join(lines)
+    
+
+def in_progress(args):
+    print("Functionality is not completed")
+    print("Arguments passed are: ")
+    print(args)
+
+
+@app.require_app_instance
+def export_instance(args, instance):
+    filename = args.filename
+    archive_format = args.archive_format
+    # make_archive(filename, archive_format, base_dir = instance)
+    in_progress(args)
+
+    
+@app.require_app_instance
+def resolve_command(args, instance):
+    defect_id =args.defect_id
+    resolution = args.resolution
+    if resolution is None:
+        resolution = get_multiline("Resolution")
+    app.resolve(defect_id, resolution, instance)
+    
+
+@app.require_app_instance
+def comment_cmd(args, instance):
+    defect_id = args.defect_id
+    comment = args.comment
+
+    if comment is None:
+        comment = get_multiline("Comment: ")
+    app.add_comment(defect_id, comment, instance)
+    
+
+@app.require_app_instance
+def status_cmd(args, instance):
+    defect_id = args.defect_id
+    def update_status():
+            app.update_status(defect_id, args.update, instance)
+            sys.exit(0)
+
+    if args.update is not None:
+        update_status()
+        
+    status = app.get_status(defect_id, instance)
+    print(status)
+
+    
+@app.require_app_instance
+def new_defect(args, instance):
+    if args.description is None:
+        description = get_multiline("Enter the description: ")
+    else:
+        description = args.description
+
+    if args.observed is None:
+        observed = get_multiline("Observed behaviour: ")
+    else:
+        observed = args.observed
+
+    if args.expected is None:
+        expected = get_multiline("Expected behaviour: ")
+    else:
+        expected = args.expected
+
+    did = app.create_new_defect(description, expected, observed, instance)
+    print("Bug created with id: ",did)
+
+    
+@app.require_app_instance
+def query_instance(args, instance):
+    # TODO: Refactor the code to split the function into several functions
+    # TODO: Refactor code to pretty print the results
+    bugs = app.get_all_bugs(instance)
+    if args.get:
+        defect_id = args.get
+        bug = app.get_bug(defect_id, instance)
+        for key, value in bug.items():
+            if key != 'comments':
+                print(key, value, sep=':  ')
+        print("Comments: ")
+        for item in bug['comments']:
+            print(item)
+        sys.exit(0)
+    if len(bugs) != 0:
+        print("Found {x} items: ".format(x = len(bugs)))
+        for item in bugs:
+            print(item)
+    else:
+        print("No item found")
+
+
+def create_app_instance(args):
+    instance = app.get_app_instance()
+    if instance is not None:
+        print("Already part of an environment: {instance}".format(instance=instance))
+    else:
+        app.create_new_instance()
+        
+
+def check_instance(args):
+    instance = app.get_app_instance()
+    if instance is not None:
+        print("Instance present at: {instance}".format(instance=instance))
+    else:
+        print("No instance exists in the current or any of the parent directories")
+
+
 def get_parser():
-    parser = ArgumentParser()
+    parser = ArgumentParser(description=DESCRIPTION, prog=PROG)
+    parser.set_defaults(func=None)
 
-    subparsers = parser.add_subparsers()
+    subcommands = parser.add_subparsers(help="Subcommands")
 
-    initor = subparsers.add_parser('init', help='initialise a bugme instance')
-    initor.set_defaults(func=initialise)
+    create_instance = subcommands.add_parser("create", help="create a bugme db instance")
+    create_instance.set_defaults(func=create_app_instance)
 
-    chcker = subparsers.add_parser('check', help='check if the current folder is part of a bugme instance')
-    chcker.set_defaults(func=check_instance)
+    check = subcommands.add_parser('check', help='check if the current folder is part of a bugme instance')
+    check.set_defaults(func=check_instance)
 
-    creator = subparsers.add_parser('create',help='Create a new defect')
-    creator.add_argument('-f','--file', help='defect argument file [for bulk creation')
-    creator.add_argument('--description', help='description of the defect')
-    creator.add_argument('--values', help='the description, expected and observed behaviour values as comma separated values')
-    creator.set_defaults(func=create_new_defect)
-    
-    getter = subparsers.add_parser('get', help='get the details of a defect')
-    getter.add_argument('defect_id', help='the id of the defect to be fetched')
-    getter.set_defaults(func=get_defect)
-    
-    updater = subparsers.add_parser('update', help='update a defect with new information')
-    updater.add_argument('defect_id', help='the id of the defect to be updated')
-    updater.add_argument('--status', help='new status to be updated')
-    updater.add_argument('--comment',help='add a comment to the defect')
-    updater.add_argument('--resolve', help='resolve the defect')
-    updater.set_defaults(func=update_defect)
-    
-    reporter = subparsers.add_parser('report', help='generate a report based on the query')
-    reporter.set_defaults(func=in_progress)
+    export = subcommands.add_parser('export', help='export the instance to an archive')
+    export.add_argument('filename', help='name of the generated archive file')
+    export.add_argument('--archive_format', default='zip', help='format of the archive')
+    export.set_defaults(func=export_instance)
+
+    query = subcommands.add_parser('query', help='query the bug database')
+    query.add_argument('--get', help='get a particular defect by id')
+    query.set_defaults(func=query_instance)
+
+    add = subcommands.add_parser('add', help='create a new defect')
+    add.add_argument('description', default=None, nargs='?')
+    add.add_argument('observed', default=None, nargs='?')
+    add.add_argument('expected', default=None, nargs='?')
+    add.set_defaults(func=new_defect)
+
+    resolve = subcommands.add_parser('resolve', help='close a defect with resolution')
+    resolve.add_argument('defect_id', help='id of the defect that is to be resolved')
+    resolve.add_argument('resolution', nargs='?', help='resolution for the defect')
+    resolve.set_defaults(func=resolve_command)
+
+    comment = subcommands.add_parser('comment', help ='add or modify comments to a defect')
+    comment.add_argument('defect_id', help='id of the defect to comment')
+    comment.add_argument('--comment', help='comment text', nargs='?')
+    comment.set_defaults(func=comment_cmd)
+
+    status = subcommands.add_parser('status', help='update the status of the bug')
+    status.add_argument('defect_id', help='id of the defect for which status is required')
+    status.add_argument('--update', help='update the status')
+    status.set_defaults(func=status_cmd)
+
+    parser.add_argument('--version', action='version',  version='%(prog)s {version}'.format(version=VERSION))
 
     return parser
 
 
-
-def initialise(args):
-    if APP_PATH is not None:
-        print("Cannot initialise instance")
-        print(f"An instance already exists at {APP_PATH}")
-        sys.exit(1)
-
-    cwd = Path.cwd()
-    app.App.create_new_instance(cwd)
-
-
-def _check_instance():
-    if APP_PATH is None:
-        print("No app instance found in the current directory (or in any of the parent directories)")
-        sys.exit(1)
-    return
-
-
-def check_instance(args):
-    print("Running check...")
-    _check_instance()
-    print("Valid app instance")
-    
-
-def create_new_defect(args, app_instance):
-    if args.file is not None:
-        print("This feature is still not implemented")
-        return
-
-    if args.values is not None:
-        description, expected, observed = args.values.split(',')
-
-    elif args.description is not None:
-        description, expected, observed  = args.description, '', ''
-
-    else:
-        description = input("Description: ")
-        expected_behaviour = input("Expected behaviour: ")
-        observed_behaviour = input("Observerd behaviour: ")
-
-    app_instance.create_new_defect(description, expected_behaviour, observed_behaviour)
-
-
-def get_defect(args, app_instance):
-    defect_id = args.defect_id
-    
-    try:
-        defect = app_instance.get_defect(defect_id)
-    except ValueError:
-        print(f"no defect with defect id '{defect_id}'")
-        sys.exit(1)
-
-    comments = defect.pop('comments')
-    for name, value in defect.items():
-        print(name.replace('_', ' ').title(), value, sep=':\t')
-
-    print("\nComments: ")
-    for comment in comments:
-        print(comment['created_date'],comment['comment'],sep=':\t')
-
-
-def update_defect(args, app_instance):
-    defect_id = args.defect_id
-
-    if args.status is None and args.comment is None and args.resolve is None:
-       print("Update what? ")
-       sys.exit(1)
-
-    if args.comment:
-        app_instance.add_comment(defect_id, args.comment)
-
-    elif args.status:
-        app_instance.update_status(defect_id, args.status)
-
-    elif args.resolve:
-        app_instance.resolve_defect(defect_id, args.resolve)
-    
-    return  
-        
-
-def in_progress(args, app_instance):
-    print("option is not implemented yet")
-    
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     parser = get_parser()
     args = parser.parse_args()
-    _check_instance()
-    app_instance = app.App(APP_PATH)
-    args.func(args, app_instance)
-    
+
+    action = args.func
+    if action is None:
+        print("Arguments not given: ")
+        parser.print_help()
+    else:
+        action(args)
